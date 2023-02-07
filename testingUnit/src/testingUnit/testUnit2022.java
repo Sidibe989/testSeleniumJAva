0eebac7f6c9144d2e641187b907dd5f3aca8d60b
sidibe989 <saliousidibe69.sidibe@gmail.com>
2023-02-07 16:47:12 +0100

    test riassuntivo

---
+++ testingUnit/src/testingUnit/testUnit2022.java
@@ -0,0 +1,15 @@
+package testingUnit;
+
+public class testUnit2022 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Ab=0;
+		int Sb=1;
+		double sommma=0;
+		sommma= Ab +Sb;
+		System.out.print(sommma);
+
+	}
+
+}
